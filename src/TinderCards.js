0b6b082b23230e347d91ea758e2b3b4f421fdcd8
marquesmolefe <marquesmolefe@gmail.com>
2021-01-26 15:25:59 +0200

    header completed

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -4,6 +4,24 @@ import './TinderCards.css';
 function TinderCards() {
     return (
         <div className="tindercards">
+            <div className='tinderCards__cardContainer'>
+                {people.map(person) => {
+                    <TinderCard
+                        className='swipe'
+                        key={person.name}
+                        preventSwipe={['up', 'down']}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div
+                            style={{ backgroundImage: `url(${person.url})` }} 
+                            className='card'
+                            >
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                }}
+            </div>
             
         </div>
     )
